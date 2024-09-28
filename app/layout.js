665a35fae0f453d076dbb669c817509979864fe9
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2024-09-28 20:49:17 +0300

    Bakım Modu Aktif!

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,35 @@ export default function RootLayout({ children, session }) {
           {/* <Navbar links={links}/> */}
           {/* <SpeedInsights />
           <Analytics /> */}
-          <div className="bg-white">{children}</div>
+          <div className="bg-white">
+            <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+              <div className="bg-white rounded-3xl shadow-lg p-8 max-w-lg text-center transform transition hover:scale-105 duration-300 ease-in-out">
+                <div className="mb-6">
+                  <svg
+                    className="w-20 h-20 mx-auto text-purple-500"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M13 16h-1v-4h-1m2 0h1v4m-1 0h1m4-5H7m4 0v2m0 0v2m0-2H7m5 0h5"
+                    />
+                  </svg>
+                </div>
+                <h2 className="text-3xl font-bold mb-4 text-gray-800">
+                  BAKIM SÜRECİNDEDİR
+                </h2>
+                <p className="text-gray-600 mb-6">
+                  Panelimiz şuan bakıma girmiştir. En kısa sürede tekrar
+                  hizmetinizde olacağız. Anlayışınız için teşekkür ederiz.
+                </p>
+              </div>
+            </div>
+          </div>
           <Footer />
           <ToastContainer />
         </Providers>
